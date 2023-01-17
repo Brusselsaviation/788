fd061fca1733da7bbf79fa4d8aa029a342034bd4
Matt3o0 <duvbolone@gmail.com>
2023-01-17 21:04:08 +0100

    SASL updates

--- plugins/SASLFree/data/modules/main.lua
+++ plugins/SASLFree/data/modules/main.lua
@@ -9,6 +9,9 @@ panel2d = false
 panel3d = true
 panelWidth3d = 2048
 panelHeight3d = 2048
+sasl.options.set3DRendering(true)
+sasl.options.setAircraftPanelRendering(true)
+sasl.options.setInteractivity(true)
 
 
 
@@ -17,7 +20,6 @@ components = {
 }
 
 function onModuleInit()
-    print(sasl.getXPVersion())
-    print(sasl.getXPlanePath())
-    print(sasl.getOS())
+    print("User is using X-Plane version", sasl.getXPVersion(), "at" , sasl.getXPlanePath(), "on", sasl.getOS())
+    print("Aircraft located at", sasl.getAircraftPath())
 end
